e06304123c094c568fc400642dff48df4c75022f
mufid1012 <mufidghibran1012@gmail.com>
2025-11-20 16:33:07 +0700

    Menambahkan route Formulir beserta pilihan gender dan navigasi submit

--- app/src/main/java/com/example/model_viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/model_viewmodel/Navigasi.kt
@@ -36,4 +36,19 @@ fun SiswaApp(
             navController = navController,
             startDestination = Navigasi.Formulir.name,
             modifier = Modifier.padding(isiRuang)
-        )
+        ){
+
+            composable(route = Navigasi.Formulir.name) {
+
+                val konteks = LocalContext.current
+
+                FormSiswa(
+                    pilihanJK = DataJK.JenisK.map { id ->
+                        konteks.resources.getString(id)
+                    },
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
