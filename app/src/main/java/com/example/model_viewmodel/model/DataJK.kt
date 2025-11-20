63f6b1b09e6d5bacfc0e66bee2371e8c28df8738
mufid1012 <mufidghibran1012@gmail.com>
2025-11-20 09:18:05 +0700

    initial commit

---
+++ app/src/main/java/com/example/model_viewmodel/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.model_viewmodel.model
+
+import com.example.model_viewmodel.R
+
+object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
---
+++ app/src/main/java/com/example/model_viewmodel/model/Siswa.kt
@@ -0,0 +1,7 @@
+package com.example.model_viewmodel.model
+
+data class Siswa (
+    val nama : String = "",
+    val gender : String = "",
+    val alamat : String = ""
+)
