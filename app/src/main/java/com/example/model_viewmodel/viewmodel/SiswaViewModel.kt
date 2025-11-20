87f19ecf09f603ee6185331e6947f36d4b4172f5
mufid1012 <mufidghibran1012@gmail.com>
2025-11-20 09:25:41 +0700

    membuat file SiswaViewModel

--- app/src/main/java/com/example/model_viewmodel/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/model_viewmodel/viewmodel/SiswaViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.model_viewmodel.viewmodel
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import com.example.model_viewmodel.model.Siswa
+
+class SiswaViewModel : ViewModel() {
+
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI
+
+    fun setSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2]
+            )
+        }
+    }
+}
